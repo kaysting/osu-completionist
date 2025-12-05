cbc08244f62164504b75a9247a7d1c46b958a69e
Kayla Kersting <kayla@kaysting.dev>
2025-12-05 03:36:11 +0000

    Add animated background

--- routes/home.js
+++ routes/home.js
@@ -3,10 +3,23 @@ const db = require('../db');
 
 const router = express.Router();
 
+const stats = {};
+const statCacheTime = 1000 * 60 * 5;
+let lastStatRefresh = 0;
+const getStats = () => {
+    const now = Date.now();
+    if (now - lastStatRefresh < statCacheTime && stats.users && stats.beatmaps && stats.passes) {
+        return stats;
+    }
+    stats.users = db.prepare('SELECT COUNT(*) AS count FROM users').get().count;
+    stats.beatmaps = db.prepare('SELECT COUNT(*) AS count FROM beatmaps').get().count;
+    stats.passes = db.prepare('SELECT COUNT(*) AS count FROM user_passes').get().count;
+    lastStatRefresh = now;
+    return stats;
+};
+
 router.get('/', (req, res) => {
-    const countUsers = db.prepare('SELECT COUNT(*) AS count FROM users').get().count;
-    const countBeatmaps = db.prepare('SELECT COUNT(*) AS count FROM beatmaps').get().count;
-    const countPasses = db.prepare('SELECT COUNT(*) AS count FROM user_passes').get().count;
+    const stats = getStats();
     res.render('layout', {
         page: 'home',
         meta: {
@@ -14,9 +27,9 @@ router.get('/', (req, res) => {
             description: `Track your osu! completionist progress across all game modes, ranked, loved, and convert maps, and compare your progress with others!`
         },
         stats: {
-            users: countUsers.toLocaleString(),
-            beatmaps: countBeatmaps.toLocaleString(),
-            passes: countPasses.toLocaleString()
+            users: stats.users.toLocaleString(),
+            beatmaps: stats.beatmaps.toLocaleString(),
+            passes: stats.passes.toLocaleString()
         },
         me: req.me
     });
