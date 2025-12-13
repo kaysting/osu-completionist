c8d673261f5278e59585d1ffb9003ff34345f8ea
Kayla Kersting <kayla@kaysting.dev>
2025-12-13 21:51:46 +0000

    Add audio previews to beatmap cards

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -10,3 +10,47 @@ const copyText = async text => {
         alert('Clipboard API is not supported in this browser.');
     }
 };
+
+let lastAudioButtonElement;
+let audioPlayer = new Audio();
+// Update button state on audio events
+audioPlayer.addEventListener('ended', () => {
+    const elBtn = lastAudioButtonElement;
+    const elIcon = lastAudioButtonElement.querySelector('.icon');
+    elIcon.innerText = 'play_arrow';
+    elBtn.dataset.playing = false;
+});
+audioPlayer.addEventListener('pause', () => {
+    const elBtn = lastAudioButtonElement;
+    const elIcon = lastAudioButtonElement.querySelector('.icon');
+    elIcon.innerText = 'play_arrow';
+    elBtn.dataset.playing = false;
+});
+audioPlayer.addEventListener('play', () => {
+    const elBtn = lastAudioButtonElement;
+    const elIcon = lastAudioButtonElement.querySelector('.icon');
+    elIcon.innerText = 'pause';
+    elBtn.dataset.playing = true;
+});
+const audioButtonClick = (event, audioUrl) => {
+    // Get clicked button
+    const elBtn = event.currentTarget;
+    // Change icon of previous button to play
+    if (lastAudioButtonElement) {
+        lastAudioButtonElement.querySelector('.icon').innerText = 'play_arrow';
+    }
+    // If the this button is the same as the last one, handle play/pause
+    if (elBtn === lastAudioButtonElement) {
+        if (elBtn.dataset.playing === "true") {
+            audioPlayer.pause();
+        } else {
+            audioPlayer.play();
+        }
+        return;
+    }
+    // Update previous button variable
+    lastAudioButtonElement = elBtn;
+    // Play audio
+    audioPlayer.src = audioUrl;
+    audioPlayer.play();
+};
