dc0763ef231eda0a4f37da9b04ca138c458eca78
Kayla Kersting <kayla@kaysting.dev>
2025-12-16 03:17:08 +0000

    Sort maps by date when no search query is provided

--- routes/search.js
+++ routes/search.js
@@ -7,7 +7,11 @@ router.get('/', (req, res) => {
     const query = req.query.q?.trim() || '';
     const limits = 12;
     const users = searchUsers(query, limits, 0);
-    const maps = searchBeatmaps(query, true, false, null, null, limits, 0);
+    const maps = searchBeatmaps(
+        query, true, false,
+        query ? null : 'date_desc',
+        null, limits, 0
+    );
     res.render('layout', {
         page: 'search',
         title: 'Search',
