7aa61a215c27d6c7d4a9797b14534611f52dec43
Kayla Kersting <kayla@kaysting.dev>
2026-01-13 04:16:42 +0000

    Fix env bug

--- helpers/env.js
+++ helpers/env.js
@@ -10,22 +10,22 @@ const env = {
     DB_BACKUP_INTERVAL_HOURS: Number(process.env.DB_BACKUP_INTERVAL_HOURS || 6),
     DB_KEEP_BACKUPS_COUNT: Number(process.env.DB_KEEP_BACKUPS_COUNT || 12),
 
-    // osu
-    OSU_CLIENT_ID: process.env.OSU_CLIENT_ID,
-    OSU_CLIENT_SECRET: process.env.OSU_CLIENT_SECRET,
-    OSU_AUTH_REDIRECT_URI: process.env.OSU_AUTH_REDIRECT_URI,
-
     // Webserver
     WEBSERVER_PORT: Number(process.env.WEBSERVER_PORT || 8080),
     JWT_SECRET: process.env.JWT_SECRET,
     SESSION_SECRET: process.env.SESSION_SECRET,
-    HOSTNAME: process.env.HOSTNAME || 'localhost:8080',
+    HOST: process.env.HOST || 'localhost:8080',
 
     // Webserver rate limits
     CLIENT_RATE_LIMIT_LIMIT: Number(process.env.CLIENT_RATE_LIMIT_LIMIT || 100),
     CLIENT_RATE_LIMIT_WINDOW_SECS: Number(process.env.CLIENT_RATE_LIMIT_WINDOW_SECS || 300),
     API_RATE_LIMIT_LIMIT: Number(process.env.API_RATE_LIMIT_LIMIT || 60),
     API_RATE_LIMIT_WINDOW_SECS: Number(process.env.API_RATE_LIMIT_WINDOW_SECS || 60),
+
+    // osu
+    OSU_CLIENT_ID: process.env.OSU_CLIENT_ID,
+    OSU_CLIENT_SECRET: process.env.OSU_CLIENT_SECRET,
+    OSU_AUTH_REDIRECT_URI: process.env.OSU_AUTH_REDIRECT_URI || `https://${process.env.HOST}/auth/callback`,
 
     // Discord
     DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN,
