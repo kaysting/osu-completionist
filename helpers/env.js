9c8c269bb853110e7bca97a4625271a96b8900b5
Kayla Kersting <kayla@kaysting.dev>
2026-01-18 06:59:29 +0000

    Relax client rate limits a bit

--- helpers/env.js
+++ helpers/env.js
@@ -18,7 +18,7 @@ env.HOST = process.env.HOST || `localhost:${env.WEBSERVER_PORT}`;
 env.HTTPS = process.env.HTTPS !== 'false';
 
 // Webserver rate limits
-env.CLIENT_RATE_LIMIT_LIMIT = Number(process.env.CLIENT_RATE_LIMIT_LIMIT || 50);
+env.CLIENT_RATE_LIMIT_LIMIT = Number(process.env.CLIENT_RATE_LIMIT_LIMIT || 150);
 env.CLIENT_RATE_LIMIT_WINDOW_SECS = Number(process.env.CLIENT_RATE_LIMIT_WINDOW_SECS || 300);
 env.API_RATE_LIMIT_LIMIT = Number(process.env.API_RATE_LIMIT_LIMIT || 60);
 env.API_RATE_LIMIT_WINDOW_SECS = Number(process.env.API_RATE_LIMIT_WINDOW_SECS || 60);
