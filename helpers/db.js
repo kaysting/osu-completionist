d502256a2edc9aa42e0e3e14d5329efc7c6ca418
Kayla Kersting <kayla@kaysting.dev>
2026-01-18 11:17:23 +0000

    Raise database busy timeout

--- helpers/db.js
+++ helpers/db.js
@@ -6,7 +6,7 @@ utils.log(`Using database ${env.DB_PATH}`);
 const db = new Database(env.DB_PATH);
 
 db.pragma('journal_mode = WAL');
-db.pragma('busy_timeout = 5000');
+db.pragma('busy_timeout = 15000');
 db.pragma('synchronous = NORMAL');
 
 module.exports = db;
