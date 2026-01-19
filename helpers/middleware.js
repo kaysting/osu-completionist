bfa0be9595089edbc075af5dc6b1db7dd8fe7b73
Kayla Kersting <kayla@kaysting.dev>
2026-01-19 07:44:37 +0000

    Only update user last login time every 5 minutes, not on every request

--- helpers/middleware.js
+++ helpers/middleware.js
@@ -33,7 +33,12 @@ const middleware = {
         req.me = null;
         if (data?.id) {
             req.me = db.prepare('SELECT * FROM users WHERE id = ?').get(data.id);
-            db.prepare(`UPDATE users SET last_login_time = ? WHERE id = ?`).run(Date.now(), data.id);
+            if (req.me) {
+                const updateLastLoginTimeAfterMs = 1000 * 60 * 5;
+                if ((Date.now() - req.me.last_login_time) > updateLastLoginTimeAfterMs) {
+                    db.prepare(`UPDATE users SET last_login_time = ? WHERE id = ?`).run(Date.now(), data.id);
+                }
+            }
         }
         next();
     },
