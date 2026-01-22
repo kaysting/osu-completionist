d8f0bd0510fa9d05113c497ec313d15284b887e5
Kayla Kersting <kayla@kaysting.dev>
2026-01-22 04:43:29 +0000

    Create function to make graph definitions reusable

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -55,3 +55,76 @@ const audioVolumeUp = () => {
     let volume = parseFloat(localStorage.getItem('mapPreview')) || 0.5;
     audioVolumeSet(volume + 0.1);
 };
+
+const renderLineGraph = (canvasElement, opts) => {
+    const ctx = canvasElement.getContext('2d');
+    const datasets = [];
+    for (const dataset of opts.datasets) {
+        const hue = dataset.hue || 150;
+        const gradient = ctx.createLinearGradient(0, 0, 0, 100);
+        gradient.addColorStop(0, `hsla(${hue}, 90%, 60%, 20%)`);
+        gradient.addColorStop(1, `hsla(${hue}, 90%, 60%, 0%)`);
+        datasets.push({
+            data: dataset.values,
+            backgroundColor: gradient,
+            borderColor: `hsl(${hue}, 90%, 60%)`,
+            borderWidth: 2,
+            pointRadius: 0,
+            fill: true,
+            tension: 0.3
+        });
+    }
+    const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: opts.labels,
+            datasets: datasets
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            // Disable the load animation but keep tooltip animations
+            animation: {
+                duration: 0
+            },
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    mode: 'index',
+                    intersect: false,
+                    displayColors: false,
+                    cornerRadius: 8,
+                    padding: 8,
+                    titleMarginBottom: 2,
+                    backgroundColor: 'hsl(220, 20%, 8%)',
+                    titleFont: {
+                        size: 12,
+                        weight: 600,
+                        family: 'Torus'
+                    },
+                    bodyFont: {
+                        size: 16,
+                        weight: 'normal',
+                        family: 'Torus'
+                    },
+                    titleColor: `hsl(220, 20%, 65%)`,
+                    bodyColor: `hsl(220, 20%, 90%)`,
+                    callbacks: {
+                        label: opts.tooltipCallbacks.label || (context => context.parsed.y.toString()),
+                        title: opts.tooltipCallbacks.title || (context => `${context[0].label}`)
+                    }
+                }
+            },
+            scales: {
+                x: { display: false },
+                y: { display: false }
+            },
+            layout: {
+                padding: 0
+            }
+        }
+    });
+    return chart;
+};
