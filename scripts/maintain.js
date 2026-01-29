6e1b162f1bbb7d602f8bacf05a2e52bfeb67f339
Kayla Kersting <kayla@kaysting.dev>
2026-01-29 10:04:09 +0000

    Add maintenance function to check a single user's recent scores for passes

--- scripts/maintain.js
+++ scripts/maintain.js
@@ -203,9 +203,19 @@ const options = [
         description: `Recalculate category stats for all users, including totals.`
     },
     {
-        f: apiWrite.savePassesFromAllUserRecents,
+        f: (userId) => {
+            if (!userId) {
+                return apiWrite.savePassesFromAllUserRecents;
+            }
+            const user = apiRead.getUserProfile(userId);
+            if (!user?.name) return console.log(`User ${userId} not found`);
+            apiWrite.savePassesFromUserRecents(userId);
+        },
         name: `saverecentpasses`,
-        description: `Save the past 24 hours of user passes using per-user recent scores.`
+        description: `Save the past 24 hours of user passes using per-user recent scores.`,
+        args: [
+            { name: 'user_id' }
+        ]
     },
     {
         f: () => console.log(`Here's a cryptographically securely random secret:\n${utils.generateSecretKey(32)}`),
